ac960b53a333abcbb86cd65a0e69f8b4cf1bcc0f
sogaiu <983021772@users.noreply.github.com>
2024-03-28 21:27:23 +0900

    Update elpaca version to 0.7

--- init.el
+++ init.el
@@ -8,13 +8,13 @@
 ;;
 ;; elpaca - https://github.com/progfolio/elpaca#installer
 
-(defvar elpaca-installer-version 0.6)
+(defvar elpaca-installer-version 0.7)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order
   '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-           :ref nil
+           :ref nil :depth 1
            :files (:defaults "elpaca-test.el" (:exclude "extensions"))
            :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -27,16 +27,19 @@
     (when (< emacs-major-version 28) (require 'subr-x))
     (condition-case-unless-debug err
         (if-let ((buffer (pop-to-buffer-same-window "*elpaca-bootstrap*"))
-                 ((zerop (call-process "git" nil buffer t "clone"
-                                       (plist-get order :repo) repo)))
+                 ((zerop (apply #'call-process
+                                `("git" nil ,buffer t "clone"
+                                  ,@(when-let ((depth (plist-get order :depth)))
+                                      (list (format "--depth=%d" depth)
+                                            "--no-single-branch"))
+                                  ,(plist-get order :repo) ,repo))))
                  ((zerop (call-process "git" nil buffer t "checkout"
                                        (or (plist-get order :ref) "--"))))
                  (emacs (concat invocation-directory invocation-name))
-                 ((zerop (call-process emacs nil buffer nil
-                                       "-Q" "-L" "." "--batch"
-                                       "--eval"
-                                       (concat "(byte-recompile-directory"
-                                               " \".\" 0 'force)"))))
+                 ((zerop (call-process
+                          emacs nil buffer nil "-Q" "-L" "." "--batch"
+                          "--eval"
+                          "(byte-recompile-directory \".\" 0 'force)")))
                  ((require 'elpaca))
                  ((elpaca-generate-autoloads "elpaca" repo)))
             (progn (message "%s" (buffer-string)) (kill-buffer buffer))
