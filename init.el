dde98f5ddea5bf09532866c1c3f59c8ba17d3408
sogaiu <983021772@users.noreply.github.com>
2024-03-28 21:27:34 +0900

    Use a different work-around for magit

--- init.el
+++ init.el
@@ -247,7 +247,10 @@
            :files ("lisp/*.el")))
 
 ;; https://github.com/progfolio/elpaca/issues/273#issuecomment-1975319923
-(elpaca (magit :tag "v3.3.0"))
+'(elpaca (magit :tag "v3.3.0"))
+
+(elpaca transient)
+(elpaca magit)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
